9d8423df61b84fdc299f887df006b99504489bd9
홍유라 <hhhyr96@gmail.com>
2022-09-09 17:29:51 +0900

    Feature : 주소록 object 생성
    - 'Address' Controller, DTO, Service, Mapper Interface 생성
    
    Close #35

---
+++ workforus/src/main/java/site/workforus/forus/address/controller/AddressController.java
@@ -0,0 +1,12 @@
+package site.workforus.forus.address.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/addr")
+public class AddressController {
+
+	
+	
+}
