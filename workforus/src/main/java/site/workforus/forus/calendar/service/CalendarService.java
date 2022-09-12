5533660fe8168987df69e192350b804b6399da0c
최영원 <youngwon0607@gmail.com>
2022-09-12 16:57:05 +0900

    feat : calendar 관련 Service 및 Controller 생성
    - calendar 관련 Service 및 Controller Class 생성
    - Calednar, CalendarShare, Schedule 에 관한 간단한 CRUD 기능 구현
    
    CLOSE #49

---
+++ workforus/src/main/java/site/workforus/forus/calendar/service/CalendarService.java
@@ -0,0 +1,129 @@
+package site.workforus.forus.calendar.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import site.workforus.forus.calendar.model.CalendarDTO;
+import site.workforus.forus.global.dto.ResponseDTO;
+import site.workforus.forus.mapper.CalendarMapper;
+
+@Service
+public class CalendarService {
+
+	@Autowired
+	private SqlSession session;
+
+	public ResponseEntity<Object> addCalendar(CalendarDTO calendarDTO) {
+
+		CalendarMapper mapper = session.getMapper(CalendarMapper.class);
+
+		boolean insertResult = mapper.insertData(calendarDTO);
+
+		ResponseDTO<CalendarDTO> result;
+
+		ResponseEntity<Object> res;
+
+		if (insertResult) {
+			result = new ResponseDTO<>("SUCCESS", 200, calendarDTO);
+			res = new ResponseEntity<>(result, HttpStatus.OK);
+		} else {
+			result = new ResponseDTO<>("FAIL", 400, "캘린더 생성에 실패하였습니다.", calendarDTO);
+			res = new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+		}
+
+		return res;
+
+	}
+
+	public ResponseEntity<Object> selectAll() {
+
+		CalendarMapper mapper = session.getMapper(CalendarMapper.class);
+
+		List<CalendarDTO> datas = mapper.selectAll();
+
+		var result = new ResponseDTO<List<CalendarDTO>>("SUCCESS", 200, datas);
+
+		ResponseEntity<Object> res = new ResponseEntity<>(result, HttpStatus.OK);
+
+		return res;
+	}
+
+	public ResponseEntity<Object> selectByCalId(int calId) {
+
+		CalendarMapper mapper = session.getMapper(CalendarMapper.class);
+
+		List<CalendarDTO> datas = mapper.selectByCalId(calId);
+
+		ResponseDTO<List<CalendarDTO>> result = new ResponseDTO<List<CalendarDTO>>("SUCCESS", 200, datas);
+
+		ResponseEntity<Object> res = new ResponseEntity<>(result, HttpStatus.OK);
+
+		return res;
+	}
+
+	public ResponseEntity<Object> selectByEmpId(String empId) {
+		CalendarMapper mapper = session.getMapper(CalendarMapper.class);
+
+		List<CalendarDTO> datas = mapper.selectByEmpId(empId);
+
+		ResponseDTO<List<CalendarDTO>> result = new ResponseDTO<List<CalendarDTO>>("SUCCESS", 200, datas);
+
+		ResponseEntity<Object> res = new ResponseEntity<>(result, HttpStatus.OK);
+
+		return res;
+	}
+
+	public ResponseEntity<Object> updateCalendar(CalendarDTO calendarDTO) {
+		CalendarMapper mapper = session.getMapper(CalendarMapper.class);
+
+		boolean updateResult = mapper.updateData(calendarDTO);
+
+		ResponseDTO<CalendarDTO> result;
+
+		ResponseEntity<Object> res;
+
+		if (updateResult) {
+			result = new ResponseDTO<>("SUCCESS", 200, calendarDTO);
+			res = new ResponseEntity<>(result, HttpStatus.OK);
+		} else {
+			result = new ResponseDTO<>("FAIL", 400, "일정 수정에 실패하였습니다.", calendarDTO);
+			res = new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+		}
+
+		return res;
+	}
+
+	public ResponseEntity<Object> deleteCalendar(int calId) {
+		CalendarMapper mapper = session.getMapper(CalendarMapper.class);
+
+		boolean deleteResult = mapper.deleteById(calId);
+
+		Map<String, Integer> map = new HashMap<>();
+
+		map.put("calId", calId);
+
+		JSONObject json = new JSONObject(map);
+
+		ResponseDTO<JSONObject> result;
+
+		ResponseEntity<Object> res;
+
+		if (deleteResult) {
+			result = new ResponseDTO<>("SUCCESS", 200, json);
+			res = new ResponseEntity<>(result, HttpStatus.OK);
+		} else {
+			result = new ResponseDTO<>("FAIL", 400, "캘린더 삭제에 실패하였습니다.", json);
+			res = new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+		}
+
+		return res;
+	}
+}
---
+++ workforus/src/main/java/site/workforus/forus/calendar/service/CalendarShareService.java
@@ -0,0 +1,121 @@
+package site.workforus.forus.calendar.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import site.workforus.forus.calendar.model.CalendarShareDTO;
+import site.workforus.forus.global.dto.ResponseDTO;
+import site.workforus.forus.mapper.CalendarShareMapper;
+
+@Service
+public class CalendarShareService {
+
+	@Autowired
+	private SqlSession session;
+
+	public ResponseEntity<Object> addCalendarShare(CalendarShareDTO calendarShareDTO) {
+
+		CalendarShareMapper mapper = session.getMapper(CalendarShareMapper.class);
+
+		boolean insertResult = mapper.insertData(calendarShareDTO);
+
+		ResponseDTO<CalendarShareDTO> result;
+
+		ResponseEntity<Object> res;
+
+		if (insertResult) {
+			result = new ResponseDTO<>("SUCCESS", 200, calendarShareDTO);
+			res = new ResponseEntity<>(result, HttpStatus.OK);
+		} else {
+			result = new ResponseDTO<>("FAIL", 400, "캘린더 공유에 실패하였습니다.", calendarShareDTO);
+			res = new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+		}
+
+		return res;
+	}
+
+	public ResponseEntity<Object> selectAll() {
+
+		CalendarShareMapper mapper = session.getMapper(CalendarShareMapper.class);
+
+		List<CalendarShareDTO> datas = mapper.selectAll();
+
+		ResponseDTO<List<CalendarShareDTO>> result = new ResponseDTO<>("SUCCESS", 200, datas);
+
+		ResponseEntity<Object> res = new ResponseEntity<>(result, HttpStatus.OK);
+
+		return res;
+	}
+
+	public ResponseEntity<Object> selectByCalId(int calId) {
+
+		CalendarShareMapper mapper = session.getMapper(CalendarShareMapper.class);
+
+		List<CalendarShareDTO> datas = mapper.selectByCalId(calId);
+
+		ResponseDTO<List<CalendarShareDTO>> result = new ResponseDTO<>("SUCCESS", 200, datas);
+
+		ResponseEntity<Object> res = new ResponseEntity<>(result, HttpStatus.OK);
+
+		return res;
+	}
+
+	public ResponseEntity<Object> selectByEmpId(String empId) {
+		CalendarShareMapper mapper = session.getMapper(CalendarShareMapper.class);
+
+		List<CalendarShareDTO> datas = mapper.selectByEmpId(empId);
+
+		ResponseDTO<List<CalendarShareDTO>> result = new ResponseDTO<>("SUCCESS", 200, datas);
+
+		ResponseEntity<Object> res = new ResponseEntity<>(result, HttpStatus.OK);
+
+		return res;
+	}
+
+	public ResponseEntity<Object> selectJoinCalByEmpId(String empId) {
+		CalendarShareMapper mapper = session.getMapper(CalendarShareMapper.class);
+
+		List<CalendarShareDTO> datas = mapper.selectJoinCalByEmpId(empId);
+
+		ResponseDTO<List<CalendarShareDTO>> result = new ResponseDTO<>("SUCCESS", 200, datas);
+
+		ResponseEntity<Object> res = new ResponseEntity<>(result, HttpStatus.OK);
+
+		return res;
+	}
+
+	public ResponseEntity<Object> deleteCalendarShare(int calShrId) {
+		CalendarShareMapper mapper = session.getMapper(CalendarShareMapper.class);
+
+		boolean deleteResult = mapper.deleteById(calShrId);
+
+		Map<String, Integer> map = new HashMap<>();
+
+		map.put("calShrId", calShrId);
+
+		JSONObject json = new JSONObject(map);
+
+		ResponseDTO<JSONObject> result;
+
+		ResponseEntity<Object> res;
+
+		if (deleteResult) {
+			result = new ResponseDTO<>("SUCCESS", 200, json);
+			res = new ResponseEntity<>(result, HttpStatus.OK);
+		} else {
+			result = new ResponseDTO<>("FAIL", 400, "캘린더 공유 삭제에 실패하였습니다.", json);
+			res = new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+		}
+
+		return res;
+	}
+
+}
