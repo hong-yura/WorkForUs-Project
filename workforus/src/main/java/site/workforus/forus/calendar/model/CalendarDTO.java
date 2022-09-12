705bd962830245b9abf9ebc8f62edc8c1429fe12
최영원 <youngwon0607@gmail.com>
2022-09-12 16:42:30 +0900

    Feature : calendar 관련 dto - calendar, calendarShare, schedule dto 생성

--- workforus/src/main/java/site/workforus/forus/calendar/model/CalendarDTO.java
+++ workforus/src/main/java/site/workforus/forus/calendar/model/CalendarDTO.java
@@ -2,8 +2,6 @@ package site.workforus.forus.calendar.model;
 
 import org.apache.ibatis.type.Alias;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,8 +9,6 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Alias("calendarDto")
 public class CalendarDTO {
 
@@ -23,5 +19,13 @@ public class CalendarDTO {
 	private String calName; // 캘린더 이름
 
 	private String calAccess; // 캘린더 공유 상태
+
+	public CalendarDTO(int calId, String empId, String calName, String calAccess) {
+		super();
+		this.calId = calId;
+		this.empId = empId;
+		this.calName = calName;
+		this.calAccess = calAccess;
+	}
 
 }
