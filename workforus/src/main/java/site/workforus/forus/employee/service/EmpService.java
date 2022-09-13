d77709bca47fcb8b52e2165098089426d0a9fdf0
김규연 <rbdus9696@gmail.com>
2022-09-13 12:24:02 +0900

    feature : spring-security object 추가, 기능 구현 (#66)

---
+++ workforus/src/main/java/site/workforus/forus/employee/service/EmpService.java
@@ -0,0 +1,57 @@
+package site.workforus.forus.employee.service;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import site.workforus.forus.employee.model.EmpDTO;
+import site.workforus.forus.employee.model.LoginVO;
+import site.workforus.forus.mapper.EmpMapper;
+
+@Service
+@EnableAspectJAutoProxy(proxyTargetClass=true)
+public class EmpService implements UserDetailsService{
+
+	@Autowired
+	private SqlSession session;
+	
+	@Autowired
+	private BCryptPasswordEncoder pwEncoder;
+	
+	public boolean signup(EmpDTO empDto) {
+		EmpMapper mapper = session.getMapper(EmpMapper.class);
+		
+		empDto.setEmpPw(pwEncoder.encode(empDto.getEmpPw()));
+		empDto.setEmpCheckPw(pwEncoder.encode(empDto.getEmpCheckPw()));
+		int result = mapper.insertEmployee(empDto);
+		
+		return result == 1 ? true : false;
+	}
+
+	@Override
+	public LoginVO loadUserByUsername(String empId) throws UsernameNotFoundException {
+		LoginVO loginVo = new LoginVO();
+		
+		EmpMapper mapper = session.getMapper(EmpMapper.class);
+		
+		EmpDTO empDto = new EmpDTO();
+		empDto.setEmpId(empId);
+		
+		empDto = mapper.selectEmployee(empDto);
+		
+		if(empDto == null) {
+			return null;
+		}
+		loginVo.setUsername(empDto.getEmpId());
+		loginVo.setPassword(empDto.getEmpPw());
+		return new LoginVO(empDto);
+	}
+	
+}
