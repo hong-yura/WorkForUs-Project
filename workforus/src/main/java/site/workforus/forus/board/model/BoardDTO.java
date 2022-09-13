412aa5981d81b1fd4c4217f716ec65b4194285a7
이보슬 <tool0923@naver.com>
2022-09-14 08:30:18 +0900

    feature : 게시글 테이블 추가 & 게시판 관련 DTO 수정
    - BoardDTO categoryId 추가
    - BoardParticipDTO에 boardId 추가
    - BoardPostDTO 추가

--- workforus/src/main/java/site/workforus/forus/board/model/BoardDTO.java
+++ workforus/src/main/java/site/workforus/forus/board/model/BoardDTO.java
@@ -11,27 +11,29 @@ import site.workforus.forus.admin.model.DeptDTO;
 @ToString
 @Alias("boardDto") 
 public class BoardDTO {
-	private int boardId;					// 게시판 id
-	private String boardNm;					// 게시판 이름
-	private String invLink;					// 게시판 url
-	private String deptYn;					// 게시판 타입
-	private DeptDTO deptObj; 				// 부서 
-	private BoardCategoryDTO categoryObj;	// 카테고리 가져올 때 사용 
-	private BoardParticipDTO participObj;	// 게시판 가져올 때 사용
-	
+	private int boardId;			// 게시판 id
+	private int categoryId;			// 카테고리 id
+	private String boardNm;			// 게시판 이름
+	private String deletedYn;		// 삭제 여부
+	private String publicYn;		// 전체공개 여부
+	private String anonymityYn;		// 익명 여부
+	private String invLink;			// 게시판 url
+	private int deptNo;				// 부서 번호 -> 어떤 부서의 게시판인지
+	private int deptYn;		// 게시판 타입
+
 	public BoardDTO() {}
-	
-	public BoardDTO(int boardId, String boardNm, String invLink, String deptYn, DeptDTO deptObj,
-			BoardCategoryDTO categoryObj, BoardParticipDTO participObj) {
+
+	public BoardDTO(int boardId, int categoryId, String boardNm, String deletedYn, String publicYn, String anonymityYn, String invLink,
+			int deptNo, int deptYn) {
 		super();
 		this.boardId = boardId;
+		this.categoryId = categoryId;
 		this.boardNm = boardNm;
+		this.deletedYn = deletedYn;
+		this.publicYn = publicYn;
+		this.anonymityYn = anonymityYn;
 		this.invLink = invLink;
+		this.deptNo = deptNo;
 		this.deptYn = deptYn;
-		this.deptObj = deptObj;
-		this.categoryObj = categoryObj;
-		this.participObj = participObj;
 	}
-	
-	
 }
--- workforus/src/main/java/site/workforus/forus/board/model/BoardParticipDTO.java
+++ workforus/src/main/java/site/workforus/forus/board/model/BoardParticipDTO.java
@@ -13,14 +13,14 @@ import lombok.ToString;
 public class BoardParticipDTO {
 	private int participNo;		// 참여 명단 번호
 	private String empId;		// 참여 사원 id
+	private int boardId;		// 게시판 id
 	
 	public BoardParticipDTO() {}
-	
-	public BoardParticipDTO(int participNo, String empId) {
+
+	public BoardParticipDTO(int participNo, String empId, int boardId) {
 		super();
 		this.participNo = participNo;
 		this.empId = empId;
+		this.boardId = boardId;
 	}
-	
-	
 }
