36d3524453781f8f822d74067f666e009bd7178e
홍유라 <hhhyr96@gmail.com>
2022-09-14 11:30:20 +0900

    Feature : 조직도 관리 조회 기능 수정중
    Close #60

--- workforus/src/main/java/site/workforus/forus/admin/controller/DeptController.java
+++ workforus/src/main/java/site/workforus/forus/admin/controller/DeptController.java
@@ -8,30 +8,44 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import site.workforus.forus.admin.model.DeptDTO;
 import site.workforus.forus.admin.service.DeptService;
 import site.workforus.forus.employee.model.EmpDTO;
 
 @Controller
-public class MembersMandgeController {
+public class DeptController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(MembersMandgeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(DeptController.class);
 	
 	@Autowired
 	private DeptService deptService;
 	
-	@RequestMapping(value = "/members", method = RequestMethod.GET)
-	public String getAll(Model model, DeptDTO deptDto, EmpDTO empDto) {
+	// 부서 전체 조회
+	@GetMapping(value = "/group_manage")
+	public String getDeptAll(Model model, DeptDTO deptDto) {
 		
-		List<DeptDTO> deptDatas = deptService.getAll(); 
+		List<DeptDTO> deptDatas = deptService.getDeptAll(); 
 		model.addAttribute("deptDatas", deptDatas);
 		logger.info("data: {}", deptDatas);
 		
-		return "admin/members";
+		return "admin/group_manage";
 	}
 	
-
+	// 부서 추가
+	@GetMapping(value = "/group_add")
+	public String addDept(Model model, DeptDTO deptDto) {
+		DeptDTO deptDatas = deptService.addDept();
+		model.addAttribute("deptDatas", deptDatas);
+		logger.info("addDept: {}", deptDatas);
+		
+		return "admin//group_add";
+	}
+	
+	
+	
 }
