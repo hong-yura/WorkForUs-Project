a1bc84784932b0348c2fd56b41e7b9e526a4c311
홍유라 <hhhyr96@gmail.com>
2022-09-14 12:14:21 +0900

    Feature : 로고 설정

---
+++ workforus/src/main/java/site/workforus/forus/admin/controller/LogoController.java
@@ -0,0 +1,25 @@
+package site.workforus.forus.admin.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/admin")
+public class LogoController {
+	
+	// private static final Logger logger = LoggerFactory.getLogger(LogoController.class);
+	
+	@GetMapping(value = "")
+	public String getPage() {
+		
+		return "admin/logo";
+		
+	}
+	
+	
+	
+}
