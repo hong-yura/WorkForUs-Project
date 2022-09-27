46b729b3db73aabf236062fa8acd66672d7550b7
김규연 <rbdus9696@gmail.com>
2022-09-27 12:13:12 +0900

    feature : 회원가입 시 이메일 인증 구현 (#168)

--- workforus/src/main/java/site/workforus/forus/employee/controller/EmpController.java
+++ workforus/src/main/java/site/workforus/forus/employee/controller/EmpController.java
@@ -10,10 +10,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import site.workforus.forus.employee.model.EmpDTO;
 import site.workforus.forus.employee.service.EmpService;
+import site.workforus.forus.employee.service.MailSendService;
 
 
 @Controller
@@ -22,6 +24,9 @@ public class EmpController {
 	
 	@Autowired
 	private EmpService service;
+	
+	@Autowired
+	private MailSendService mailService;
 	
 	@GetMapping(value="/login")
 	public String login(Model model) {
@@ -61,5 +66,13 @@ public class EmpController {
 	@GetMapping(value="/login/fail") 
 	public String loginFail(Model model) {
 		return "login/login_fail";
+	}
+	
+	@GetMapping(value="/mailCheck")
+	@ResponseBody
+	public String mailCheck(String email) {
+		System.out.println("이메일 인증 요청 들어옴!");
+		System.out.println("이메일 인증 이메일: " + email);
+		return mailService.joinEmail(email);
 	}
 }
