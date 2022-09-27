b971e0e36f1ec6591689061ae7467178d7e0659a
김재은 <bbori530@naver.com>
2022-09-27 10:16:47 +0900

    refactor : 월급조회 js분리

---
+++ workforus/src/main/webapp/resources/js/pages/commute/salary.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+ 
+	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+	  return new bootstrap.Tooltip(tooltipTriggerEl)
+	});
