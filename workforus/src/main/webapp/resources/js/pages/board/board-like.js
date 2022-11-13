ae76eb1793418b2cd60bd3516b87895f5168a3b5
이보슬 <tool0923@naver.com>
2022-11-13 14:55:49 +0900

    feature : 게시글 추천 오류 해결
    - 게시글 좋아요를 누를 시 추천수가 변경되지 않는 오류를 해결하였다.
    - ajax의 success에서 data를 전달받으면 data.likeCnt 와 같이 값을 꺼내서 사용할 수 있다.

--- workforus/src/main/webapp/resources/js/pages/board/board-like.js
+++ workforus/src/main/webapp/resources/js/pages/board/board-like.js
@@ -3,7 +3,7 @@
  */
 const boardLike = () => {
     const postId = $("#postId").val();
-    const likeCnt = $("#id_like");
+    const likeCnt = $("#id_like")[0];
     console.log(postId);
 
     $.ajax({
@@ -11,10 +11,8 @@ const boardLike = () => {
         url: "/post/like",
         data: JSON.stringify({"postId":postId}),
         contentType:"application/json; charset=UTF-8",
-        success: function(data){
-            console.log(data);
-            console.log(likeCnt);
-
+        success: function(data, message){
+            likeCnt.innerText = data.likeCnt;
         },
         error: function (){
             alert("다시 시도해주세요.");
