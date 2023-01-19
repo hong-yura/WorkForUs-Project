8f84a5c30366f486deff0850ff13a674653fd23f
김규연 <rbdus9696@gmail.com>
2023-01-19 09:05:36 +0900

    feature : setAllowedOrigins 추가

--- workforus/src/main/java/site/workforus/forus/chat/config/StompWebSocketConfig.java
+++ workforus/src/main/java/site/workforus/forus/chat/config/StompWebSocketConfig.java
@@ -12,7 +12,7 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stomp/chat")
-                .setAllowedOrigins("http://localhost:8080")
+                .setAllowedOrigins("https://workforus.site")
                 .withSockJS();
     }
 
